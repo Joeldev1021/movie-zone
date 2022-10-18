54956fd888e03abc618f537876d6c5df5eaf6356
joeldev <joelpimentel1995@gmail.com>
2022-10-18 11:36:31 -0500

    fix: GET movies ⚙️

--- src/hooks/useGetMovies.tsx
+++ src/hooks/useGetMovies.tsx
@@ -5,21 +5,24 @@ import { IMovie } from "../interface/movie.interface";
 
 interface Props {
   path: string;
-  query?: string;
+  params?: {
+    query?: string;
+    with_genres?: string;
+  };
 }
 
-export const useGetMovies = ({ path, query }: Props) => {
+export const useGetMovies = ({ path, params }: Props) => {
   const [movies, setMovies] = useState<IMovie[]>([]);
   const [error, setError] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     setLoading(true);
-    API.get(path, { params: { query } })
+    API.get(path, { params: { ...params } })
       .then((response) => setMovies(movieMapper(response.data.results)))
       .catch((error) => setError(true))
       .finally(() => setLoading(false));
-  }, [path, query]);
+  }, [params?.query, params?.with_genres]);
 
   return { movies, loading, error };
 };
